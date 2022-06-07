7501075190c07dac46cbf657535065ef36442a59
Emeraldnickel <Emeraldnickel@gmail.com>
2022-06-07 12:24:52 +1000

    Streamlined FearFacing

--- src/main/java/net/mcreator/dreamsandnightmares/procedures/FearFacingProcedure.java
+++ src/main/java/net/mcreator/dreamsandnightmares/procedures/FearFacingProcedure.java
@@ -20,22 +20,31 @@ public class FearFacingProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
+		boolean found = false;
 		double blockX = 0;
 		double blockY = 0;
 		double blockZ = 0;
+		double sx = 0;
+		double sy = 0;
+		double sz = 0;
+		double count = 0;
 		if ((world instanceof Level _lvl ? _lvl.dimension() : Level.OVERWORLD) == (ResourceKey.create(Registry.DIMENSION_REGISTRY,
 				new ResourceLocation("dreams_and_nightmares:nightmare_dimension")))) {
 			if (entity instanceof Player _player && !_player.level.isClientSide())
 				_player.displayClientMessage(new TextComponent("You faced your fears with a clear mind."), (false));
-			blockX = x;
-			blockY = y;
-			blockZ = z;
+			sx = -5;
+			found = false;
+			count = 0;
 			for (int index0 = 0; index0 < (int) (10); index0++) {
+				sy = -5;
 				for (int index1 = 0; index1 < (int) (10); index1++) {
+					sz = -5;
 					for (int index2 = 0; index2 < (int) (10); index2++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
+						if ((world.getBlockState(new BlockPos(x + sx, y + sy, z + sz))).getBlock() == DreamsAndNightmaresModBlocks.NIGHTMARE_GEM_BLOCK
+								.get()) {
+							found = true;
 							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
+								BlockPos _bp = new BlockPos(x, y, z);
 								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
 								BlockState _bso = world.getBlockState(_bp);
 								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
@@ -48,211 +57,23 @@ public class FearFacingProcedure {
 								}
 								world.setBlock(_bp, _bs, 3);
 							}
-							blockX = blockX + 1;
 						}
+						sz = sz + 1;
 					}
-					blockX = blockX - 10;
-					blockZ = blockZ + 1;
+					sy = sy + 1;
 				}
-				blockZ = blockZ - 10;
-				blockY = blockY + 1;
+				sx = sx + 1;
 			}
-			blockY = blockY - 10;
-			for (int index3 = 0; index3 < (int) (10); index3++) {
-				for (int index4 = 0; index4 < (int) (10); index4++) {
-					for (int index5 = 0; index5 < (int) (10); index5++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX + 1;
-						}
-					}
-					blockX = blockX - 10;
-					blockZ = blockZ + 1;
-				}
-				blockZ = blockZ - 10;
-				blockY = blockY - 1;
+			if (found == true) {
+				if (entity instanceof Player _player && !_player.level.isClientSide())
+					_player.displayClientMessage(new TextComponent(("Successfully replaced " + count + " blocks!")), (false));
+			} else {
+				if (entity instanceof Player _player && !_player.level.isClientSide())
+					_player.displayClientMessage(new TextComponent("But you had no fears to face."), (false));
 			}
-			blockY = blockY + 10;
-			for (int index6 = 0; index6 < (int) (10); index6++) {
-				for (int index7 = 0; index7 < (int) (10); index7++) {
-					for (int index8 = 0; index8 < (int) (10); index8++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX + 1;
-						}
-					}
-					blockX = blockX - 10;
-					blockZ = blockZ - 1;
-				}
-				blockZ = blockZ + 10;
-				blockY = blockY - 1;
-			}
-			blockY = blockY + 10;
-			for (int index9 = 0; index9 < (int) (10); index9++) {
-				for (int index10 = 0; index10 < (int) (10); index10++) {
-					for (int index11 = 0; index11 < (int) (10); index11++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX - 1;
-						}
-					}
-					blockX = blockX + 10;
-					blockZ = blockZ - 1;
-				}
-				blockZ = blockZ + 10;
-				blockY = blockY - 1;
-			}
-			blockY = blockY + 10;
-			for (int index12 = 0; index12 < (int) (10); index12++) {
-				for (int index13 = 0; index13 < (int) (10); index13++) {
-					for (int index14 = 0; index14 < (int) (10); index14++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX - 1;
-						}
-					}
-					blockX = blockX + 10;
-					blockZ = blockZ + 1;
-				}
-				blockZ = blockZ - 10;
-				blockY = blockY - 1;
-			}
-			blockY = blockY + 10;
-			for (int index15 = 0; index15 < (int) (10); index15++) {
-				for (int index16 = 0; index16 < (int) (10); index16++) {
-					for (int index17 = 0; index17 < (int) (10); index17++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX + 1;
-						}
-					}
-					blockX = blockX - 10;
-					blockZ = blockZ + 1;
-				}
-				blockZ = blockZ - 10;
-				blockY = blockY + 1;
-			}
-			blockY = blockY - 10;
-			for (int index18 = 0; index18 < (int) (10); index18++) {
-				for (int index19 = 0; index19 < (int) (10); index19++) {
-					for (int index20 = 0; index20 < (int) (10); index20++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX - 1;
-						}
-					}
-					blockX = blockX + 10;
-					blockZ = blockZ - 1;
-				}
-				blockZ = blockZ + 10;
-				blockY = blockY + 1;
-			}
-			blockY = blockY - 10;
-			for (int index21 = 0; index21 < (int) (10); index21++) {
-				for (int index22 = 0; index22 < (int) (10); index22++) {
-					for (int index23 = 0; index23 < (int) (10); index23++) {
-						if ((world.getBlockState(new BlockPos(blockX, blockY, blockZ))).getBlock() == DreamsAndNightmaresModBlocks.FEAR.get()) {
-							{
-								BlockPos _bp = new BlockPos(blockX, blockY, blockZ);
-								BlockState _bs = DreamsAndNightmaresModBlocks.RESILIENCE.get().defaultBlockState();
-								BlockState _bso = world.getBlockState(_bp);
-								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
-									Property _property = _bs.getBlock().getStateDefinition().getProperty(entry.getKey().getName());
-									if (_property != null && _bs.getValue(_property) != null)
-										try {
-											_bs = _bs.setValue(_property, (Comparable) entry.getValue());
-										} catch (Exception e) {
-										}
-								}
-								world.setBlock(_bp, _bs, 3);
-							}
-							blockX = blockX - 1;
-						}
-					}
-					blockX = blockX + 10;
-					blockZ = blockZ + 1;
-				}
-				blockZ = blockZ - 10;
-				blockY = blockY + 1;
-			}
+		} else {
+			if (entity instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(new TextComponent("But you weren't having a nightmare..."), (false));
 		}
 	}
 }
